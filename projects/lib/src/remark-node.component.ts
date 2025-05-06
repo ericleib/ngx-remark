5532cece6e924fb488716fba40e94c54b8ba47d2
Eric Leibenguth <eric.leibenguth@gmail.com>
2025-05-06 21:32:26 +0200

    Simplify trackBy syntax and get children with computed()

--- projects/lib/src/remark-node.component.ts
+++ projects/lib/src/remark-node.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, TrackByFunction, inject, input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, computed, inject, input } from "@angular/core";
 import { Node, Parent } from "mdast";
 import { RemarkTemplatesService } from "./remark-templates.service";
 
@@ -13,15 +13,10 @@ export class RemarkNodeComponent {
 
   readonly node = input.required<Node>({ alias: "remarkNode" });
 
-  get children() {
-    return (this.node() as Parent).children;
-  }
+  readonly children = computed(() => (this.node() as Parent).children)
 
   get templates() {
     return this.templateService.templates;
   }
 
-  // Tracking by index means that DOM elements are reused even when the node type and content is different
-  // Changes should still be reflected because of the @Input() node binding
-  trackBy: TrackByFunction<Node> = (index) => index;
 }
